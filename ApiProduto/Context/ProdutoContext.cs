5acaa7d2383b283f0e91b0de618c54292313bef5
JeeFzPiires <jeferson.piires@gmail.com>
2021-11-23 21:24:09 -0300

    TDE Produtos - Swagger

---
+++ ApiProduto/Context/ProdutoContext.cs
@@ -0,0 +1,60 @@
+﻿using ApiProduto.models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProduto.Context
+{
+    public class ProdutoContext
+    {
+
+        public MongoDatabase Database;
+        public String DataBaseName = "test";
+        string conexaoMongoDB = "mongodb+srv://TDE:tde@cluster0.nxwxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+        public IMongoCollection<Produto> _produtos;
+        public IMongoCollection<Usuario> _usuarios;
+        public ProdutoContext()
+        {
+            var cliente = new MongoClient(conexaoMongoDB);
+            var server = cliente.GetDatabase(DataBaseName);
+            _produtos = server.GetCollection<Produto>("Produtos");
+        }
+    }
+    public class CategoriaContext
+    {
+
+        public MongoDatabase Database;
+        public String DataBaseName = "test";
+        string conexaoMongoDB = "mongodb+srv://TDE:tde@cluster0.nxwxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+
+        public IMongoCollection<Categoria> _categorias;
+        public CategoriaContext()
+        {
+            var cliente = new MongoClient(conexaoMongoDB);
+            var server = cliente.GetDatabase(DataBaseName);
+            _categorias = server.GetCollection<Categoria>("Categorias");
+        }
+    }
+
+    public class UsuarioContext
+    {
+        public MongoDatabase Database;
+        public String DataBaseName = "test";
+        string conexaoMongoDB = "mongodb+srv://TDE:tde@cluster0.nxwxm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+        public IMongoCollection<Usuario> _usuarios;
+
+        public UsuarioContext()
+        {
+            var cliente = new MongoClient(conexaoMongoDB);
+            var server = cliente.GetDatabase(DataBaseName);
+            _usuarios = server.GetCollection<Usuario>("Usuarios");
+        }
+    }
+}
+
